28491e056576a19f062952f6ed13a15fc0d718c7
kev <yqlbu@bu.edu>
2023-12-14 19:07:58 +0800

    feat(webhook): add service relay

--- src/api/v1/endpoints/webhook.py
+++ src/api/v1/endpoints/webhook.py
@@ -1,4 +1,6 @@
 import json
+import os
+from httpx import AsyncClient, HTTPError
 from fastapi import APIRouter, Request, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
@@ -6,10 +8,26 @@ from fastapi.encoders import jsonable_encoder
 router = APIRouter()
 
 
+async def forward(url):
+    async with AsyncClient() as client:
+        return await client.get(url)
+
+
 @router.post("/webhook")
 async def webhook(request: Request):
     serialized_json = json.dumps(await request.json())
-    return JSONResponse(
-        content=jsonable_encoder({"payload": json.loads(serialized_json)}),
-        status_code=status.HTTP_200_OK,
-    )
+
+    try:
+        res = await forward(os.getenv("RELAY_SERVICE_ENDPOINT", ""))
+        if res.status_code == status.HTTP_200_OK:
+            return JSONResponse(
+                content=jsonable_encoder(json.loads(serialized_json)),
+                status_code=status.HTTP_200_OK,
+            )
+        else:
+            raise HTTPError("failed to relay message to designated service")
+    except HTTPError as err:
+        return JSONResponse(
+            content=jsonable_encoder({"error": str(err)}),
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
