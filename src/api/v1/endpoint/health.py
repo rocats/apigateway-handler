4343d05d6417dee2c46cd2d95c861700027af290
kev <yqlbu@bu.edu>
2023-12-17 23:42:45 +0800

    patch: use post method ONLY for all routes

--- src/api/v1/endpoint/health.py
+++ src/api/v1/endpoint/health.py
@@ -5,7 +5,7 @@ from fastapi.encoders import jsonable_encoder
 router = APIRouter()
 
 
-@router.get("/health")
+@router.post("/health")
 async def health():
     return JSONResponse(
         content=jsonable_encoder({"health": "ok!"}), status_code=status.HTTP_200_OK
