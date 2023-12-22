d321c774b1d2ff7601dc7a632baf977acf0cce06
kev <yqlbu@bu.edu>
2023-12-22 21:22:55 +0800

    feat: add relay functionality

--- src/api/v1/endpoint/webhook.py
+++ src/api/v1/endpoint/webhook.py
@@ -1,7 +1,6 @@
 import structlog
 from uuid import uuid4
-from httpx import AsyncClient
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from cloudevents.http import CloudEvent
@@ -11,20 +10,16 @@ router = APIRouter()
 logger = structlog.get_logger()
 
 
-async def forward(url):
-    async with AsyncClient() as client:
-        return await client.get(url)
-
-
 @router.post("/webhook")
-async def webhook():
+async def webhook(request: Request):
+    payload = await request.json()
     attributes = {
         "id": str(uuid4()),
         "type": "dev.knative.staging.repeater-v3.webhook-relay",
         "source": "dev.knative.staging/repeater-v3/apigateway-interceptor",
     }
-    data = {"message": "forwarded payload message to relay service successfully!"}
-    event = CloudEvent(attributes, data)
+    logger.debug("Forwarded payload message to relay service successfully!")
+    event = CloudEvent(attributes, payload)
     headers, body = to_binary(event)
 
     return JSONResponse(
